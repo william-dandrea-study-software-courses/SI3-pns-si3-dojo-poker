061346ee09c43ebd410f73f1a8301714906fc1ed
Amine-hub787 <71392330+Amine-hub787@users.noreply.github.com>
2020-10-04 16:10:37 +0200

    rectification

--- src/interaction/Victory.java
+++ src/interaction/Victory.java
@@ -6,54 +6,49 @@ import interaction.ResultType;
 import java.util.HashMap;
 
 public class Victory {
-    private HashMap<ResultType,String> types = new HashMap<ResultType,String>(){{
-        put(ResultType.brelan,"Brelan");
-        put(ResultType.carre,"carré");
-        put(ResultType.couleur,"couleur");
-        put(ResultType.doublePair,"double pair");
-        put(ResultType.full,"Full");
-        put(ResultType.higherCard,"Higher Card");
-        put(ResultType.pair,"Pair");
-        put(ResultType.quinteFlush,"Quinte Flush");
-        put(ResultType.suite,"Suite");
+    private HashMap<ResultType, String> types = new HashMap<ResultType, String>() {{
+        put(ResultType.brelan, "Brelan");
+        put(ResultType.carre, "carré");
+        put(ResultType.couleur, "couleur");
+        put(ResultType.doublePair, "double pair");
+        put(ResultType.full, "Full");
+        put(ResultType.higherCard, "Higher Card");
+        put(ResultType.pair, "Pair");
+        put(ResultType.quinteFlush, "Quinte Flush");
+        put(ResultType.suite, "Suite");
     }};
     private Victorieu victorieu;
-    private ResultType handWinType, handLoseType;
-    private Card cardWinner, cardLoser;
+    private ResultType handWinType;
+    private Card cardWinner;
 
-    public Victory(Victorieu victorieu, ResultType handWinType, ResultType handLoseType, Card cardWinner, Card cardLoser){
+    public Victory(Victorieu victorieu, ResultType handWinType, ResultType handLoseType, Card cardWinner, Card cardLoser) {
         this.victorieu = victorieu;
         this.handWinType = handWinType;
         this.cardWinner = cardWinner;
-        this.cardLoser = cardLoser;
-        this.handLoseType = handLoseType;
+
 
     }
 
-    public Victorieu getWinner(){
+    public Victorieu getWinner() {
         return victorieu;
     }
-    public ResultType getWinType(){
+
+    public ResultType getWinType() {
         return handWinType;
     }
-    public ResultType getLoseType(){
-        return handLoseType;
-    }
+
 
     @Override
     public String toString() {
-        if (victorieu == Victorieu.egalite){
-            return "il y a égalité avec "+ types.get(handWinType) + " " + cardWinner.toString() ;
-        }
-        else {
-            if (victorieu == Victorieu.main1)
-            {
-                return "La main 1 a gagnée " + types.get(handWinType) + " " + cardWinner.toString() + ", la main 2 a perdue " + types.get(handLoseType) + " " + cardLoser.toString();
-            }
-            else {
-                return "La main 2 a gagnée " + types.get(handWinType) + " " + cardWinner.toString() + ", la main 1 a perdue " + types.get(handLoseType) + " " + cardLoser.toString();
+        if (victorieu == Victorieu.egalite) {
+            return "il y a égalité avec " + types.get(handWinType) + " " + cardWinner.toString();
+        } else {
+            if (victorieu == Victorieu.main1) {
+                return "La main 1 gagne par " + types.get(handWinType) + ": " + cardWinner.toString();
+            } else {
+                return "La main 2 gagne par " + types.get(handWinType) + ": " + cardWinner.toString();
             }
         }
-    }
 
+    }
 }
